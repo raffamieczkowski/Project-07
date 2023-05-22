d1b18b025552cb39a4761932a947bc6ce6c75d81
Paweł Szopiński <szopinskipawel@gmail.com>
2023-05-22 15:25:10 +0200

    v. 0.1 (MVP)
    First page only, release date and genre id's aren't proper

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './js/example';
 import { API_KEY } from './js/api-key';
+const trendingUrl = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+const moviesEL = document.querySelector('.movies-list');
+
+async function fetchMovies() {
+  await fetch(`${trendingUrl}&api_key=${API_KEY}&page=1`)
+    .then(response => {
+      return response.json();
+    })
+    .then(movies => {
+      movies.results.forEach(movie => {
+        const markup = `<div class="movie-card">
+        <a href="#"> <img class="movie-poster" src="https://image.tmdb.org/t/p/original${movie.poster_path}" /></a>
+        <ul class="movie-short-descr">
+          <li class="movie-title">${movie.original_title}</li>
+          <li class="movie-genre">${movie.release_date} | ${movie.genre_ids}</li>
+        </ul>
+      </div>`;
+        moviesEL.insertAdjacentHTML('beforeend', markup);
+      });
+    })
+    .catch(error => console.error(error));
+}
+fetchMovies();
