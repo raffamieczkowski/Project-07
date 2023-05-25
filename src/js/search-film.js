0dd8fc74a06920dde770e43d4f493abe4450c037
NataliaRakoczy <natalia.rakoczy54@gmail.com>
2023-05-25 15:09:19 +0200

    added listener

--- src/js/search-film.js
+++ src/js/search-film.js
@@ -1,69 +1,31 @@
-import { API_KEY } from './api-key';
-import { getGenres } from './genres';
-import Notiflix from 'notiflix';
 
 
-function searchMovies(query) {
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}&language=en-US`;
+const API_KEY = '4c48f56c563cad897bb4af72634cd546';
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const movies = data.results;
-      displayMovies(movies); 
-      showNotification(movies.length); 
-    })
-    .catch(error => {
-      console.error('Wystąpił błąd:', error);
-    });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.header__form');
+  const input = document.querySelector('.form__input');
+  form.addEventListener('submit', handleFormSubmit);
 
-function displayMovies(movies) {
-  const navigationList = document.querySelector('.header__navigation');
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const searchTerm = input.value.trim();
+    if (searchTerm !== '') {
+      searchMovies(searchTerm);
+    }
+  }
 
-
-  navigationList.innerHTML = '';
-
-  movies.forEach(movie => {
-    const { title, id } = movie;
-
-    const listItem = document.createElement('li');
-    listItem.textContent = title;
-
-   
-    getGenres(id)
-      .then(genres => {
-        const genreMarkup = genres.join(', ');
-        const genreElement = document.createElement('span');
-        genreElement.textContent = genreMarkup;
-        listItem.appendChild(genreElement);
+  function searchMovies(searchTerm) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      searchTerm,
+    )}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results); // Do something with the movie results
       })
       .catch(error => {
-        console.error('Wystąpił błąd podczas pobierania gatunków:', error);
+        console.log('An error occurred:', error);
       });
-
-    navigationList.appendChild(listItem);
-  });
-}
-
-//liczba znalezionych filmów
-function showNotification(movieCount) {
-  Notiflix.Notify.success(`Znaleziono ${movieCount} filmów.`);
-}
-
-
-function handleFormSubmit(event) {
-  event.preventDefault(); 
-
-  const input = document.querySelector('.form__input');
-  const query = input.value;
-
-  if (query) {
-    searchMovies(query); 
-    input.value = ''; 
   }
-}
-
-
-const form = document.querySelector('.header__form');
-form.addEventListener('submit', handleFormSubmit);
+});
