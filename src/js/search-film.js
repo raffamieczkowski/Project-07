335a6162d018f3f24f3258fec6f87f37dc8b652b
rafalbarczak <rafal.barczak@intmail.pl>
2023-05-26 20:17:05 +0200

    created pagination

--- src/js/search-film.js
+++ src/js/search-film.js
@@ -1,10 +1,11 @@
-import { API_KEY } from "./api-key";
-import { getGenres } from "./genres";
-
+import { API_KEY } from './api-key';
+import { getGenres } from './genres';
+import { createPagination } from './pagination';
+const resultContainer = document.querySelector('.result-container');
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.header__form');
   const input = document.querySelector('.form__input');
-  const resultContainer = document.querySelector('.result-container');
+
   const searchButton = document.querySelector('.form__button');
 
   form.addEventListener('submit', handleFormSubmit);
@@ -17,66 +18,56 @@ document.addEventListener('DOMContentLoaded', () => {
       searchMovies(searchTerm);
     }
   }
-
-  async function searchMovies(searchTerm) {
-    const apiKey = API_KEY;
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(searchTerm)}`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const movies = data.results;
-
-      const genrePromises = movies.map(movie => getGenres(movie.id));
-      const genres = await Promise.all(genrePromises);
-
-      movies.forEach((movie, index) => {
-        movie.genres = genres[index];
-      });
-
-      displayMovies(movies);
-    } catch (error) {
-      console.log('An error occurred:', error);
-    }
-  }
-
-  function displayMovies(results) {
-    resultContainer.innerHTML = '';
-
-    results.forEach(movie => {
-      const movieCard = createMovieCard(movie);
-      resultContainer.appendChild(movieCard);
-    });
-  }
-
-  function createMovieCard(movie) {
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('movie__card');
-
-    const movieLink = document.createElement('a');
-    movieLink.href = '#';
-
-    const moviePoster = document.createElement('img');
-    moviePoster.classList.add('movie__poster');
-    moviePoster.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-    movieLink.appendChild(moviePoster);
-
-    const movieShortDescr = document.createElement('ul');
-    movieShortDescr.classList.add('movie__short-descr');
-
-    const movieTitle = document.createElement('li');
-    movieTitle.classList.add('movie__title');
-    movieTitle.textContent = movie.original_title;
-    movieShortDescr.appendChild(movieTitle);
-
-    const movieGenre = document.createElement('li');
-    movieGenre.classList.add('movie__genre');
-    movieGenre.textContent = `${movie.genres} | ${movie.release_date.slice(0, 4)}`;
-    movieShortDescr.appendChild(movieGenre);
-
-    movieCard.appendChild(movieLink);
-    movieCard.appendChild(movieShortDescr);
-
-    return movieCard;
-  }
 });
+
+async function searchMovies(searchTerm) {
+  const apiKey = API_KEY;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+    searchTerm,
+  )}`;
+
+  (async () => {
+    createPagination(url);
+  })();
+}
+
+function displayMovies(results) {
+  resultContainer.innerHTML = '';
+
+  results.forEach(movie => {
+    const movieCard = createMovieCard(movie);
+    resultContainer.appendChild(movieCard);
+  });
+}
+
+function createMovieCard(movie) {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie__card');
+
+  const movieLink = document.createElement('a');
+  movieLink.href = '#';
+
+  const moviePoster = document.createElement('img');
+  moviePoster.classList.add('movie__poster');
+  moviePoster.src = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+  movieLink.appendChild(moviePoster);
+
+  const movieShortDescr = document.createElement('ul');
+  movieShortDescr.classList.add('movie__short-descr');
+
+  const movieTitle = document.createElement('li');
+  movieTitle.classList.add('movie__title');
+  movieTitle.textContent = movie.original_title;
+  movieShortDescr.appendChild(movieTitle);
+
+  const movieGenre = document.createElement('li');
+  movieGenre.classList.add('movie__genre');
+  movieGenre.textContent = `${movie.genres} | ${movie.release_date.slice(0, 4)}`;
+  movieShortDescr.appendChild(movieGenre);
+
+  movieCard.appendChild(movieLink);
+  movieCard.appendChild(movieShortDescr);
+
+  return movieCard;
+}
+export { displayMovies };
