58faf6695bddb4faf4c4b0ca14652f51283b7e06
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-05-30 13:01:53 +0200

    fixed trailer, cookie, dark-mode

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,15 +1,20 @@
 const darkMode = document.querySelector('.header__toggle');
 const body = document.body;
 
-const isDarkModeEnabled = localStorage.getItem('darkModeEnabled');
-if (isDarkModeEnabled) {
-  body.classList.add('dark-mode');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const isDarkModeEnabled = localStorage.getItem('darkModeEnabled');
+
+  if (isDarkModeEnabled === 'true') {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+  }
+});
 
 darkMode.addEventListener('click', e => {
   e.preventDefault();
   body.classList.toggle('dark-mode');
 
   const isDarkMode = body.classList.contains('dark-mode');
-  localStorage.setItem('darkModeEnabled', isDarkMode);
+  localStorage.setItem('darkModeEnabled', isDarkMode.toString());
 });
